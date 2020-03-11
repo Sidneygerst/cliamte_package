abeb18b1ab001e5d243913d6a89f21cc97aaa6b6
sidney gerst <you@example.com>
2020-03-11 16:38:39 -0700

    created data and function for calc_rain

---
+++ R/calc_rain.R
@@ -0,0 +1,33 @@
+#' Average Percipitation 1995-2015
+#'
+#' This is a function that determines the wet and dry year between 1995 and 2015
+#' @param rain_data is from data within this package that has the average rainfall between 1995-2015
+#' @example calc_rain(rain_data)
+#' @author Sidney
+#' @return rain_matrix (max and min rain years and their average)
+
+calc_rain <- function(rain_data){
+
+  library(tidyverse)
+  #find the max and min precipitation
+  min_rain = min(rain_data$avg_precip)
+  dry_year = rain_data %>%
+    filter(avg_precip == min_rain) %>%
+    select(year) %>%
+    as.numeric()
+
+  max_rain = max(rain_data$avg_precip)
+  wet_year = rain_data %>%
+    filter(avg_precip == max_rain) %>%
+    select(year) %>%
+    as.numeric()
+
+  rain_matrix <- matrix(c(dry_year, min_rain, wet_year, max_rain),
+                        nrow = 2,
+                        ncol = 2,
+                        byrow = TRUE,
+                        dimnames = list(c("Min", "Max"), c("Year", "Average RF")))
+
+  return(rain_matrix)
+
+}
---
+++ R/rain_data.R
@@ -0,0 +1,12 @@
+#' Rain Data from Sidney
+#'
+#' Data from how much average rain existed from 1995-2015
+#'
+#' @format A data frame with 20 rows and 3 columns
+#' \describe{
+#' \item{year}{year}
+#' \item{avg_precip}{average amount of rain that year (inches)}
+#' \item{anomaly}{how different that year was from the average}
+#' }
+#'
+"rain_data"
